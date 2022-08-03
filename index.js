d9d13be466bc368edcd89631d42844c9d1d6ca49
Hamid Ali <hammali99@gmail.com>
2022-08-03 22:49:33 +0500

    JavaScript implemintation complete

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const bookData = document.getElementById("data");
 const submit = document.getElementById("submit");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
+const item = document.getElementById("items");
+const bookAdd = document.getElementById("add");
+const contact = document.getElementById("contact");
+const addBook = document.getElementById("Added");
+const listedBook = document.getElementById("listed");
+const contactedBook = document.getElementById("contacted");
+const message = document.getElementById("msg");
 
 document.addEventListener("DOMContentLoaded", () => {
   // Declare global variables
@@ -49,17 +56,47 @@ document.addEventListener("DOMContentLoaded", () => {
       bookData.innerHTML = str;
     }
 
+    bookList() {
+      listed.classList.remove("hide-list");
+      addBook.classList.remove("show-addbook");
+      contactedBook.classList.remove("show-contact");
+      bookAdd.style.color = "black";
+      item.style.color = "blue";
+      contact.style.color = "black";
+    }
+
+    bookAdd() {
+      listed.classList.add("hide-list");
+      addBook.classList.add("show-addbook");
+      contactedBook.classList.remove("show-contact");
+      bookAdd.style.color = "blue";
+      item.style.color = "black";
+      contact.style.color = "black";
+    }
+
+    contactAdd() {
+      listed.classList.add("hide-list");
+      addBook.classList.remove("show-addbook");
+      contactedBook.classList.add("show-contact");
+      bookAdd.style.color = "black";
+      item.style.color = "black";
+      contact.style.color = "blue";
+    }
+
     // Add books in array and save in localStorage
     addBook() {
       const bookTitle = title.value;
       const bookAuthor = author.value;
       const book = new Book(bookTitle, bookAuthor);
+      // message.classList.add('remove-msg')
 
       // Check if title and author field is empty or not
       if (title.value === "") {
-        alert("Please enter a book title");
+        message.innerHTML = "Please enter a book title";
+        title.style.border = '3px solid red';
       } else if (author.value === "") {
-        alert("Please enter a book author");
+        message.innerHTML = "Please enter a book author";
+        author.style.border = '2px solid red';
       } else {
         if (localStorage.getItem("book") === null) {
           bookArr.push(book);
@@ -69,6 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
           bookArr = JSON.parse(bookArrStr);
           bookArr.push(book);
           localStorage.setItem("book", JSON.stringify(bookArr));
+         
         }
         title.value = "";
         author.value = "";
@@ -76,6 +114,10 @@ document.addEventListener("DOMContentLoaded", () => {
         bookData.innerHTML = str;
         this.showBook();
       }
+      setTimeout(() => {
+        message.style.display = 'none';
+        message.style.border = '2px solid black';
+      }, 2500);
     }
 
     // Remove the book data by id
@@ -100,6 +142,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const book = new Book();
     book.removeBook(id);
   };
+
+  item.addEventListener("click", () => {
+    const book = new Book();
+    book.bookList();
+  });
+
+  bookAdd.addEventListener("click", () => {
+    const book = new Book();
+    book.bookAdd();
+  });
+
+  contact.addEventListener("click", () => {
+    const book = new Book();
+    book.contactAdd();
+  });
 
   const book = new Book();
   book.showBook();
