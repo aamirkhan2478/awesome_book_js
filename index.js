5427c8dac1551c77f82cc2eab48c2a6cf2063fe1
Aamir Khan <aamir.khan2478@gmail.com>
2022-08-03 23:38:54 +0500

    Add validation aditional feature

--- index.js
+++ index.js
@@ -56,48 +56,28 @@ document.addEventListener("DOMContentLoaded", () => {
       bookData.innerHTML = str;
     }
 
-    bookList() {
-      listed.classList.remove("hide-list");
-      addBook.classList.remove("show-addbook");
-      contactedBook.classList.remove("show-contact");
-      bookAdd.style.color = "black";
-      item.style.color = "blue";
-      contact.style.color = "black";
-    }
-
-    bookAdd() {
-      listed.classList.add("hide-list");
-      addBook.classList.add("show-addbook");
-      contactedBook.classList.remove("show-contact");
-      bookAdd.style.color = "blue";
-      item.style.color = "black";
-      contact.style.color = "black";
-    }
-
-    contactAdd() {
-      listed.classList.add("hide-list");
-      addBook.classList.remove("show-addbook");
-      contactedBook.classList.add("show-contact");
-      bookAdd.style.color = "black";
-      item.style.color = "black";
-      contact.style.color = "blue";
-    }
-
     // Add books in array and save in localStorage
     addBook() {
       const bookTitle = title.value;
       const bookAuthor = author.value;
       const book = new Book(bookTitle, bookAuthor);
-      // message.classList.add('remove-msg')
 
       // Check if title and author field is empty or not
       if (title.value === "") {
-        message.innerHTML = "Please enter a book title";
-        title.style.border = '3px solid red';
+        message.classList.add("show-msg");
+        message.style.color = "red";
+        title.style.border = "1px solid red";
+        author.style.border = "3px solid black";
+        message.innerHTML = `Please enter a book title`;
       } else if (author.value === "") {
-        message.innerHTML = "Please enter a book author";
-        author.style.border = '2px solid red';
+        message.classList.add("show-msg");
+        message.style.color = "red";
+        author.style.border = "1px solid red";
+        title.style.border = "3px solid black";
+        message.innerHTML = `Please enter a book author`;
       } else {
+        author.style.border = "3px solid black";
+        title.style.border = "3px solid black";
         if (localStorage.getItem("book") === null) {
           bookArr.push(book);
           localStorage.setItem("book", JSON.stringify(bookArr));
@@ -106,7 +86,6 @@ document.addEventListener("DOMContentLoaded", () => {
           bookArr = JSON.parse(bookArrStr);
           bookArr.push(book);
           localStorage.setItem("book", JSON.stringify(bookArr));
-         
         }
         title.value = "";
         author.value = "";
@@ -115,8 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
         this.showBook();
       }
       setTimeout(() => {
-        message.style.display = 'none';
-        message.style.border = '2px solid black';
+        message.classList.remove("show-msg");
       }, 2500);
     }
 
@@ -129,6 +107,36 @@ document.addEventListener("DOMContentLoaded", () => {
       str = "";
       bookData.innerHTML = str;
       this.showBook();
+    }
+
+    // Show only the book lists section
+    bookList() {
+      listed.classList.remove("hide-list");
+      addBook.classList.remove("show-addbook");
+      contactedBook.classList.remove("show-contact");
+      bookAdd.style.color = "black";
+      item.style.color = "blue";
+      contact.style.color = "black";
+    }
+
+    // Show only the Add book section
+    bookAdd() {
+      listed.classList.add("hide-list");
+      addBook.classList.add("show-addbook");
+      contactedBook.classList.remove("show-contact");
+      bookAdd.style.color = "blue";
+      item.style.color = "black";
+      contact.style.color = "black";
+    }
+
+    // Show only the contact section
+    contactAdd() {
+      listed.classList.add("hide-list");
+      addBook.classList.remove("show-addbook");
+      contactedBook.classList.add("show-contact");
+      bookAdd.style.color = "black";
+      item.style.color = "black";
+      contact.style.color = "blue";
     }
   }
 
